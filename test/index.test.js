30fd21e21c1fe51c1d71bd8a68e88a08661adff4
Bassel Dagher <basseldagher@Bassels-Mac-mini.local>
2015-03-02 18:33:47 -0500

    Initial commit

---
+++ test/index.test.js
@@ -0,0 +1,39 @@
+'use strict';
+
+// Third party libraries
+var expect = require('chai').expect;
+var rewire = require('rewire');
+
+
+// Rewire libraries
+var Graphite = rewire('../index');
+var GraphiteClient = rewire('../node_modules/graphite/lib/GraphiteClient');
+
+// Constants
+var ENV = 'stage';
+var APPLICATION = 'bbApp';
+var SERVER_NAME = 'bbServer';
+var DETAIL_METRIC_PATH = 'bb.eventsCounte';
+
+
+describe('index.js', function () {
+    describe('#Graphite(environment, application, serverName, callback)',
+        function () {
+            it('should initialize Graphite properly', function (done) {
+                var graphite = new Graphite(ENV, APPLICATION, SERVER_NAME, function (err) {
+                    expect(err).to.not.exist;
+
+                    return done();
+                });
+            });
+
+            it('should error when trying to initialize Graphite with empty parameters', function (done) {
+                var graphite = new Graphite(null, APPLICATION, SERVER_NAME, function (err) {
+                    expect(err).to.exist;
+
+                    return done();
+                });
+            });
+        }
+    );
+});
